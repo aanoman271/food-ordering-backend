97f30b9ff7d8a07aeb04a00743ed8390bbc19d06
noman291 <aanoman291@gmail.com>
2026-06-28 02:05:36 +0600

    food and order model

---
+++ models/food.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const foodSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    category: { type: String, required: true },
+    price: { type: Number, required: true },
+    image: { type: String, required: true },
+    description: { type: String },
+  },
+  { timestamps: true },
+);
+
+export default mongoose.model("Food", foodSchema);
---
+++ models/order.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const orderSchema = new mongoose.Schema(
+  {
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    items: [
+      {
+        food: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Food",
+          required: true,
+        },
+        quantity: { type: Number, required: true },
+      },
+    ],
+    totalAmount: { type: Number, required: true },
+    paymentStatus: {
+      type: String,
+      enum: ["Pending", "Paid", "Failed"],
+      default: "Pending",
+    },
+    orderStatus: {
+      type: String,
+      enum: ["Pending", "Preparing", "Delivered"],
+      default: "Pending",
+    },
+    payhereOrderId: { type: String },
+    payherePaymentId: { type: String },
+  },
+  { timestamps: true },
+);
+
+export default mongoose.model("Order", orderSchema);
