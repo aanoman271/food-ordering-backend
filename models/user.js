8265f4b149a1353c3504f1654558400a5fd2d016
noman291 <aanoman291@gmail.com>
2026-06-28 01:58:25 +0600

    setup backend with necessary pakage ,user model ,just added controlers folder

---
+++ models/user.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    phone: { type: String, required: true },
+    role: { type: String, enum: ["customer", "admin"], default: "customer" },
+  },
+  { timestamps: true },
+);
+
+export default mongoose.model("User", userSchema);
